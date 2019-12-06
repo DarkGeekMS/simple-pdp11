83dea6f400c14d553b8f80072e2a9e80d0559285
Mahmoud Adas <mido3ds@gmail.com>
2019-12-06 15:37:02 +0200

    control_unit_hardwired compiles

--- micro-inst/gencu.py
+++ micro-inst/gencu.py
@@ -47,43 +47,43 @@ def out_ones(line: str):
     res = []
     for x in line.split(', '):
         if x == 'IF NOT (Z=1) THEN END':
-            res.append("if not (zero_flag = '1') then end_flag := 1; end if;")
+            res.append("if not (zero_flag = '1') then end_flag <= 1; end if;")
         elif x == 'IF NOT (Z=0) THEN END':
-            res.append("if not (zero_flag = '0') then end_flag := 1; end if;")
+            res.append("if not (zero_flag = '0') then end_flag <= 1; end if;")
         elif x == 'IF NOT (C=0) THEN END':
-            res.append("if not (carry_flag = '0') then end_flag := 1; end if;")
+            res.append("if not (carry_flag = '0') then end_flag <= 1; end if;")
         elif x == 'IF NOT (C=1) THEN END':
-            res.append("if not (carry_flag = '1') then end_flag := 1; end if;")
+            res.append("if not (carry_flag = '1') then end_flag <= 1; end if;")
         elif x == 'IF NOT (C=0 or Z=1) THEN END':
             res.append(
-                "if not (carry_flag = '0' or zero_flag = '1') then end_flag := 1; end if;")
+                "if not (carry_flag = '0' or zero_flag = '1') then end_flag <= 1; end if;")
         elif x == 'IF NOT (C=1 or Z=1) THEN END':
             res.append(
-                "if not (carry_flag = '1' or zero_flag = '1') then end_flag := 1; end if;")
+                "if not (carry_flag = '1' or zero_flag = '1') then end_flag <= 1; end if;")
         elif x in instr['signals']:
             res.append(f"{fix_sig(x)} <= '1';")
-        elif x == 'HLT':
-            res.append(f"hlt := 1;")
 
     return ' '.join(res)
 
 
 def get_function(inst_name: str, fixed_inst_name: str, inst_lines: [str]):
     sp = ' '*4
-    func = f'{sp}-- {inst_name}'
-    func += f'\n{sp}procedure {fixed_inst_name} is'
-    func += f'\n{sp}begin'
+    func = f'{sp*2}-- {inst_name}'
+    func += f'\n{sp*2}procedure {fixed_inst_name} is'
+    func += f'\n{sp*2}begin'
 
-    func += f'\n{sp*2}case timer is'
+    if inst_name == 'HLT':
+        func += f"\n{sp*3}hlt_flag <= 1;"
+    else:
+        func += f'\n{sp*3}case timer is'
 
-    for i, line in enumerate(inst_lines[:-1]):
-        func += f'\n{sp*3}when {i} => {out_ones(line)}'
+        for i, line in enumerate(inst_lines[:-1]):
+            func += f'\n{sp*4}when {i} => {out_ones(line)}'
+        func += f'\n{sp*4}when others => end_flag <= 1;'
 
-    func += f'\n{sp*3}when others => end_flag := 1;'
+        func += f'\n{sp*3}end case;'
 
-    func += f'\n{sp*2}end case;'
-
-    return func + f'\n{sp}end procedure;'
+    return func + f'\n{sp*2}end procedure;'
 
 
 functions = '\n\n'.join([get_function(inst_name, fix_inst(inst_name), inst_lines)
@@ -107,14 +107,14 @@ end entity;
 
 architecture rtl of control_unit_hardwired is
     -- if set, do no operation
-    signal hlt: integer := 0;
+    signal hlt_flag: integer := 0;
     signal end_flag: integer := 0;
 
     -- from 0 -> n-1, where n is the number of lines for the microprogram
     signal timer: integer := 0;
 
     -- increment number of clk ticks, when it reachs MAX_CLK_TICKS increment timer and reset clk_ticks
-    constant MAX_CLK_TICKS := 4;
+    constant MAX_CLK_TICKS: integer := 4;
     signal clk_ticks: integer := 0;
 
     -- TODO: fix these assumptions according to the scheme
@@ -123,38 +123,37 @@ architecture rtl of control_unit_hardwired is
     alias neg_flag: std_logic is flags_data(2);
     alias par_flag: std_logic is flags_data(3);
     alias overfl_flag: std_logic is flags_data(4);
-
-    procedure zero_all_out is
-    begin
-        {' '.join([x for x in map(lambda x: f"{x} <= '0';", sigs)])}
-    end procedure;
-
-    procedure execute_instr is
-    begin
-        case ir_data is
-            -- TODO generate 'when cases' useing opcodes
-        end case;
-    end procedure;
-
-{functions}
 begin
 
     process (clk)
+        procedure zero_all_out is
+        begin
+            {' '.join([x for x in map(lambda x: f"{x} <= '0';", sigs)])}
+        end procedure;
+
+        procedure execute_instr is
+        begin
+            --case ir_data is
+                -- TODO generate 'when cases' useing opcodes
+            --end case;
+        end procedure;
+
+{functions}
     begin
-        if hlt = 0 and rising_edge(clk) then
-            clk_ticks := clk_ticks + 1;
+        if hlt_flag = 0 and rising_edge(clk) then
+            clk_ticks <= clk_ticks + 1;
 
             if clk_ticks = MAX_CLK_TICKS then
-                clk_ticks := 0;
+                clk_ticks <= 0;
 
-                zero_all_out();
-                execute_instr();
+                zero_all_out;
+                execute_instr;
             end if;
 
-            if end = 1 then
-                end_flag := 0;
-                timer := 0;
-                zero_all_out();
+            if end_flag = 1 then
+                end_flag <= 0;
+                timer <= 0;
+                zero_all_out;
             end if;
         end if;
     end process;
