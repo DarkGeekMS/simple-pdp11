ca17a8a5c01a200c56242cc37d76bf81bec4de4d
Mahmoud Adas <mido3ds@gmail.com>
2019-12-07 18:58:34 +0200

    collected scripts into one folder

---
+++ scripts/gencu.py
@@ -0,0 +1,220 @@
+import sys
+import json
+
+path = sys.argv[1]
+instr = {}
+
+with open(path) as f:
+    instr = json.load(f)
+
+
+def fix_sig(sig):
+    sig = sig.replace('.', '_')
+    sig = sig.replace('=', '_eq_')
+    sig = sig.replace('(', '_')
+    sig = sig.replace(')', '_')
+    sig = sig.replace('+', '_plus_')
+    sig = sig.replace('-', '_minus_')
+    sig = sig.replace('|', '_or_')
+    sig = sig.replace('~', '_not_')
+    sig = sig.replace('&', '_not_')
+    sig = sig.replace('__', '_')
+    sig = sig.lower()
+
+    return sig
+
+
+def fix_inst(inst: str):
+    inst = inst.lower()
+    inst = inst.replace(' ', '_')
+    inst = inst.replace('(', '_')
+    inst = inst.replace(')', '_')
+    inst = inst.replace('(', '_')
+    inst = inst.replace('@', '_at_')
+    inst = inst.replace('+', '_plus_')
+    inst = inst.replace('-', '_minus_')
+
+    inst = inst.replace('__', '_')
+    inst = inst.replace('__', '_')
+    inst = inst.strip('_')
+
+    return inst
+
+
+sigs = [fix_sig(sig) for sig in instr['signals']]
+
+
+def out_ones(line: str):
+    res = []
+    for x in line.split(', '):
+        if x == 'IF NOT (Z=1) THEN END':
+            res.append("if not (zero_flag = '1') then end_flag <= 1; end if;")
+        elif x == 'IF NOT (Z=0) THEN END':
+            res.append("if not (zero_flag = '0') then end_flag <= 1; end if;")
+        elif x == 'IF NOT (C=0) THEN END':
+            res.append("if not (carry_flag = '0') then end_flag <= 1; end if;")
+        elif x == 'IF NOT (C=1) THEN END':
+            res.append("if not (carry_flag = '1') then end_flag <= 1; end if;")
+        elif x == 'IF NOT (C=0 or Z=1) THEN END':
+            res.append(
+                "if not (carry_flag = '0' or zero_flag = '1') then end_flag <= 1; end if;")
+        elif x == 'IF NOT (C=1 or Z=1) THEN END':
+            res.append(
+                "if not (carry_flag = '1' or zero_flag = '1') then end_flag <= 1; end if;")
+        elif x in instr['signals']:
+            res.append(f"{fix_sig(x)} <= '1';")
+
+    return ' '.join(res)
+
+
+def get_function(inst_name: str, fixed_inst_name: str, inst_lines: [str]):
+    sp = ' '*4
+    func = f'{sp*2}-- {inst_name}'
+    func += f'\n{sp*2}procedure {fixed_inst_name} is'
+    func += f'\n{sp*2}begin'
+
+    if inst_name == 'HLT':
+        func += f"\n{sp*3}hlt_flag <= 1;"
+    else:
+        func += f'\n{sp*3}case timer is'
+
+        for i, line in enumerate(inst_lines[:-1]):
+            func += f'\n{sp*4}when {i} => {out_ones(line)}'
+        func += f'\n{sp*4}when others => end_flag <= 1;'
+
+        func += f'\n{sp*3}end case;'
+
+    return func + f'\n{sp*2}end procedure;'
+
+
+functions = '\n\n'.join([get_function(inst_name, fix_inst(inst_name), inst_lines)
+                         for (inst_name, inst_lines) in instr['instructions'].items()])
+
+
+opcodes = {
+    'MOV': '0001',
+    'ADD': '0010',
+    'ADC': '0011',
+    'SUB': '0100',
+    'SUBC': '0101',
+    'AND': '0110',
+    'OR': '0111',
+    'XNOR': '1000',
+    'CMP': '1001',
+    'INC': '11110000',
+    'DEC': '11110001',
+    'CLR': '11110010',
+    'INV': '11110011',
+    'LSR': '11110100',
+    'ROR': '11110101',
+    'RRC': '11110110',
+    'ASR': '11110111',
+    'LSL': '11111000',
+    'ROL': '11111001',
+    'RLC': '11111010',
+    'BR': '000000',
+    'BEQ': '000001',
+    'BNE': '000010',
+    'BLO': '000011',
+    'BLS': '110000',
+    'BHI': '110001',
+    'BHS': '110011',
+    'HLT': '1010',
+    'NOP': '1011',
+    'JSR': '110100',
+    'RTS': '111001',
+    'INT': '111010',
+    'IRET': '111011',
+}
+
+modes = {
+    'R': '000',
+    '(R)+': '001',
+    '-(R)': '010',
+    'X(R)': '011',
+    '@R': '100',
+    '@(R)+': '101',
+    '@-(R)': '110',
+    '@X(R)': '111',
+}
+
+
+def get_conditions(instr_parts):
+    code = opcodes[instr_parts[0]]
+
+    if len(instr_parts) == 3:
+        return f'ir_data(15 downto {16-len(code)}) = "{code}" and ir_data(11 downto 9) = "{modes[instr_parts[1]]}" and ir_data(5 downto 3) = "{modes[instr_parts[2]]}"'
+    elif len(instr_parts) == 2:
+        return f'ir_data(15 downto {16-len(code)}) = "{code}" and ir_data(11 downto 9) = "{modes[instr_parts[1]]}"'
+    else:
+        return f'ir_data(15 downto {16-len(code)}) = "{code}"'
+
+
+def get_instr_case(instr_name, fixed_name):
+    instr_name = instr_name.split()
+    sp = ' '*4*3
+    return f'{sp}if {get_conditions(instr_name)} then {fixed_name}; end if;'
+
+
+instr_cases = '\n'.join([get_instr_case(inst_name, fix_inst(inst_name))
+                         for (inst_name, _) in instr['instructions'].items()])
+
+out = f'''-- Auto generated file, any edits will be lost --
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity control_unit_hardwired is
+    port (
+        clk: in std_logic;
+        ir_data, flags_data: in std_logic_vector(15 downto 0);
+
+        {', '.join(sigs)}: out std_logic
+    );
+end entity; 
+
+
+architecture rtl of control_unit_hardwired is
+    -- if set, do no operation
+    signal hlt_flag: integer := 0;
+
+    -- indicates the end of micro-program
+    signal end_flag: integer := 0;
+
+    -- from 0 -> n-1, where n is the number of lines for the microprogram
+    signal timer: integer := 0;
+
+    alias carry_flag: std_logic is flags_data(0);
+    alias zero_flag: std_logic is flags_data(1);
+    alias neg_flag: std_logic is flags_data(2);
+    alias par_flag: std_logic is flags_data(3);
+    alias overfl_flag: std_logic is flags_data(4);
+begin
+
+    process (clk)
+        procedure zero_all_out is
+        begin
+            {' '.join([x for x in map(lambda x: f"{x} <= '0';", sigs)])}
+        end procedure;
+
+{functions}
+
+        procedure execute_instr is
+        begin
+{instr_cases}
+        end procedure;
+    begin
+        if hlt_flag = 0 and rising_edge(clk) then
+            zero_all_out;
+            execute_instr;
+            timer <= timer + 1;
+            if end_flag = 1 then
+                end_flag <= 0;
+                timer <= 0;
+            end if;
+        end if;
+    end process;
+end architecture;'''
+
+with open(sys.argv[2] + 'control_unit_hardwired.vhdl', 'w') as f:
+    print(out, file=f)
---
+++ scripts/micro.py
@@ -0,0 +1,634 @@
+def fetch_value_TMP(mode: str, tmp: str):
+    if mode == 'R':
+        return 'R'
+
+    elif mode == '(R)+':
+        wr(f'R.out, {tmp}.in, ALU.r+1')
+        wr(f'ALU.out, R.in')
+
+        return tmp
+
+    elif mode == '-(R)':
+        wr('R.out, ALU.r-1')
+        wr('ALU.out, R.in')
+
+        return 'R'
+
+    elif mode == 'X(R)':
+        # get x
+        wr('PC.out, MAR.in, RD')
+
+        # pc++
+        wr('PC.out, ALU.r+1')
+        wr('ALU.out, PC.in')
+
+        # tmp = x+R
+        wr(f'MDR.out, TMP0.in')
+        # mdr = [tmp]
+        wr(f'R.out, ALU.c=0, ALU.r+l+c')
+        wr(f'ALU.out, MAR.in, RD')
+
+        # temp = mdr TODO: maybe not optimal?
+        wr(f'MDR.out, {tmp}.in')
+
+        return tmp
+
+    elif mode == '@R':
+        wr('R.out, MAR.in, RD')
+        wr(f'MDR.out, {tmp}.in')
+
+        return tmp
+
+    elif mode == '@(R)+':
+        wr('R.out, MAR.in, RD')
+        wr(f'MAR.out, ALU.r+1')
+        wr(f'ALU.out, R.in')
+        wr(f'MDR.out, {tmp}.in')
+
+        return tmp
+
+    elif mode == '@-(R)':
+        wr(f'R.out, ALU.r-1')
+        wr(f'ALU.out, R.in, MAR.in, RD')
+        wr(f'MDR.out, {tmp}.in')
+
+        return tmp
+
+    elif mode == '@X(R)':
+        # get x
+        wr('PC.out, MAR.in, RD')
+
+        # pc++
+        wr('PC.out, ALU.r+1')
+        wr('ALU.out, PC.in')
+
+        # tmp = x+R
+        wr(f'MDR.out, TMP0.in')
+        # get tmp
+        wr(f'R.out, ALU.c=0, ALU.r+l+c')
+        wr(f'ALU.out, MAR.in, RD')
+
+        # get tmp
+        wr(f'MDR.out, MAR.in, RD')
+
+        wr(f'MDR.out, {tmp}.in')
+
+        return tmp
+
+
+def fetch_addr(mode: str, tmp: str):
+    if mode == '@R':
+        wr('R.out, MAR.in')
+
+    elif mode == '@(R)+':
+        wr('R.out, MAR.in')
+        wr(f'MAR.out, ALU.r+1')
+        wr(f'ALU.out, R.in')
+
+    elif mode == '@-(R)':
+        wr(f'R.out, ALU.r-1')
+        wr(f'ALU.out, R.in, MAR.in')
+
+    elif mode == '@X(R)':
+        # get x
+        wr('PC.out, MAR.in, RD')
+
+        # pc++
+        wr('PC.out, ALU.r+1')
+        wr('ALU.out, PC.in')
+
+        # tmp = x+R
+        wr(f'MDR.out, TMP0.in')
+        # get tmp
+        wr(f'R.out, ALU.c=0, ALU.r+l+c')
+        wr(f'ALU.out, MAR.in, RD')
+
+        wr(f'MDR.out, MAR.in')
+
+
+def _write_R(mode: str, need_addr=False):
+    if need_addr:
+        fetch_addr(mode, 'TMP0')
+
+    if mode in ('R', '-(R)', '(R)+'):
+        wr('R(src).out, R(dst).in')
+    else:
+        wr('R(src).out, MDR.in, WR')
+
+
+def _write_ALU(mode: str, need_addr=False):
+    if mode in ('R', '-(R)', '(R)+'):
+        wr('ALU.out, R(dst).in')
+    else:
+        if need_addr:
+            wr('ALU.out, TMP1.in')
+            fetch_addr(mode, 'TMP0')
+            wr('TMP1.out, MDR.in, WR')
+        else:
+            wr('ALU.out, MDR.in, WR')
+
+
+def _write_TMP(mode: str, inp: str, need_addr=False):
+    if mode in ('R', '-(R)', '(R)+'):
+        wr(f'{inp}.out, R.in')
+    else:
+        if need_addr:
+            tmp = 'TMP0' if inp != 'TMP0' else 'TMP1'
+            fetch_addr(mode, tmp)
+
+        wr(f'{inp}.out, MDR.in, WR')
+
+
+def write(mode: str, inp: str, need_addr=False):
+    if inp == 'R':
+        _write_R(mode, need_addr)
+    elif inp == 'ALU':
+        _write_ALU(mode, need_addr)
+    else:  # TMP#
+        _write_TMP(mode, inp, need_addr)
+
+#############################
+
+
+def mov(src, dst):
+    tmp1 = fetch_value_TMP(src, 'TMP1')
+    write(dst, tmp1, True)
+
+
+def add(src, dst, func='ALU.c=0, ALU.r+l+c'):
+    tmp1 = fetch_value_TMP(src, 'TMP1')
+    tmp0 = fetch_value_TMP(dst, 'TMP0')
+
+    if tmp1 == 'R' and tmp0 == 'R':
+        wr(f'R(src).out, TMP0.in')
+        wr(f'R(dst).out, {func}')
+        wr(f'ALU.out, R(src).in')
+
+    elif tmp1 == 'R':
+        wr(f'R(src).out, {func}')
+        write(dst, 'ALU')
+
+    elif tmp0 == 'R':
+        wr(f'TMP1.out, TMP0.in')
+        wr(f'R(dst).out, {func}')
+        write(dst, 'ALU')
+
+    else:
+        wr(f'{tmp1}.out, {func}')
+        write(dst, 'ALU')
+
+
+def adc(src, dst):
+    return add(src, dst, 'ALU.r+l+c')
+
+
+def sub(src, dst, func='ALU.c=0, ALU.r-l-c', do_write=True):
+    tmp1 = fetch_value_TMP(src, 'TMP1')
+    tmp0 = fetch_value_TMP(dst, 'TMP0' if tmp1 != 'R' else 'TMP1')
+
+    if tmp0 == 'R' and tmp1 == 'R':
+        wr(f'R(src).out, TMP0.in')
+        wr(f'R(dst).out, {func}')
+
+    elif tmp0 == 'R':
+        # TMP1 carries src, R(dst) carries dst
+        wr(f'TMP1.out, TMP0.in')
+        wr(f'R(dst).out, {func}')
+
+    elif tmp1 == 'R':
+        # R(src) carries src, TMP1 carries dst
+        wr(f'R(src).out, TMP0.in')
+        wr(f'TMP1.out, {func}')
+
+    else:
+        # TMP1 carries src, TMP0 carries dst
+        wr(f'ALU.c=0, ALU.r+l+c')
+        wr(f'TMP1.out, TMP0.in')
+        wr(f'ALU.out, TMP1.in')
+
+        # TMP0 carries src, TMP1 carries dst
+        wr(f'TMP1.out, {func}')
+
+    if do_write:
+        write(dst, 'ALU')
+
+
+def subc(src, dst):
+    return sub(src, dst, 'ALU.r-l-c', True)
+
+
+def andd(src, dst):
+    return add(src, dst, 'ALU.r&l')
+
+
+def orr(src, dst):
+    return add(src, dst, 'ALU.r|l')
+
+
+def xnor(src, dst):
+    return add(src, dst, 'ALU.r(XNOR)l')
+
+
+def cmpp(src, dst):
+    return sub(src, dst, 'ALU.c=0, ALU.r-l-c', False)
+
+
+def inc(dst, func='ALU.r+1'):
+    tmp = fetch_value_TMP(dst, 'TMP1')
+
+    if tmp == 'R':
+        wr(f'R.out, {func}')
+    else:
+        wr(f'{tmp}.out, {func}')
+
+    write(dst, 'ALU')
+
+
+def dec(dst):
+    return inc(dst, 'ALU.r-1')
+
+
+def clr(dst):
+    if dst in ('R', '-(R)', '(R)+'):
+        wr('ALU.zero, R(dst).in')
+
+    elif dst == 'X(R)':
+        # get x
+        wr('PC.out, MAR.in, RD')
+
+        # pc++
+        wr('PC.out, ALU.r+1')
+        wr('ALU.out, PC.in')
+
+        # [x+R] = inp
+        wr('MDR.out, TMP0.in')
+        wr('R.out, ALU.c=0, ALU.r+l+c')
+        wr(f'ALU.out, MAR.in')
+
+        wr('ALU.zero, MDR.in, WR')
+
+    elif dst == '@R':
+        wr('R(dst).out, MAR.in')
+
+        wr('ALU.zero, MDR.in, WR')
+
+    elif dst == '@(R)+':
+        wr('R(dst).out, MAR.in')
+
+        wr('ALU.zero, MDR.in, WR')
+
+        wr('R.out, ALU.r+1')
+        wr('ALU.out, R.in')
+
+        wr('ALU.zero, MDR.in, WR')
+
+    elif dst == '@-(R)':
+        wr('R(dst).out, ALU.r-1')
+        wr(f'ALU.out, MAR.in')
+        wr('ALU.out, R.in')
+
+        wr('ALU.zero, MDR.in, WR')
+
+    elif dst == '@X(R)':
+        # get x
+        wr('PC.out, MAR.in, RD')
+
+        # pc++
+        wr('PC.out, ALU.r+1')
+        wr('ALU.out, PC.in')
+
+        # tmp = x+R
+        wr('MDR.out, TMP0.in')
+        wr('R.out, ALU.c=0, ALU.r+l+c')
+        wr(f'ALU.out, MAR.in')
+
+        wr('ALU.zero, MDR.in, WR')
+
+
+def inv(dst):
+    return inc(dst, 'ALU.~r')
+
+
+def lsr(dst, func='ALU.c=0, ALU.rrc'):
+    if dst in ('R', '(R)+'):
+        wr(f'R.out, {func}')
+        wr(f'ALU.out, R.in')
+
+    elif dst == '-(R)':
+        wr('R.out, ALU.r-1')
+        wr(f'{func}, ALU.out, R.in')
+
+    elif dst == 'X(R)':
+        # get x
+        wr('PC.out, MAR.in, RD')
+
+        # pc++
+        wr('PC.out, ALU.r+1')
+        wr('ALU.out, PC.in')
+
+        # tmp = x+R
+        wr(f'MDR.out, TMP0.in')
+        # mdr = [tmp]
+        wr(f'R.out, ALU.c=0, ALU.r+l+c')
+        wr(f'ALU.out, MAR.in, RD')
+
+        wr(f'MDR.out, {func}')
+        wr(f'ALU.out, MDR.in, WR')
+
+    elif dst == '@R':
+        wr('R.out, MAR.in, RD')
+        wr(f'MDR.out, {func}')
+        wr(f'ALU.out, MDR.in, WR')
+
+    elif dst == '@(R)+':
+        wr('R.out, MAR.in, RD')
+        wr(f'MAR.out, ALU.r+1')
+        wr(f'ALU.out, R.in')
+        wr(f'MDR.out, {func}')
+        wr(f'ALU.out, MDR.in, WR')
+
+    elif dst == '@-(R)':
+        wr(f'R.out, ALU.r-1')
+        wr(f'ALU.out, R.in, MAR.in, RD')
+        wr(f'MDR.out, {func}')
+        wr(f'ALU.out, MDR.in, WR')
+
+    elif dst == '@X(R)':
+        # get x
+        wr('PC.out, MAR.in, RD')
+
+        # pc++
+        wr('PC.out, ALU.r+1')
+        wr('ALU.out, PC.in')
+
+        # tmp = x+R
+        wr(f'MDR.out, TMP0.in')
+
+        # get tmp
+        wr(f'R.out, ALU.c=0, ALU.r+l+c')
+        wr(f'ALU.out, MAR.in, RD')
+
+        # get tmp
+        wr(f'MDR.out, MAR.in, RD')
+
+        wr(f'MDR.out, {func}')
+        wr(f'ALU.out, MDR.in, WR')
+
+
+def ror(dst):
+    return lsr(dst, 'ALU.ror')
+
+
+def rrc(dst):
+    return lsr(dst, 'ALU.rrc')
+
+
+def asr(dst):
+    return lsr(dst, 'ALU.asr')
+
+
+def lsl(dst):
+    return lsr(dst, 'ALU.c=0, ALU.rlc')
+
+
+def rol(dst):
+    return lsr(dst, 'ALU.rol')
+
+
+def rlc(dst):
+    return lsr(dst, 'ALU.rlc')
+
+
+def branch(cond: str):
+    if cond is not None:
+        wr(f'IF NOT {cond} THEN END, ', end='')
+
+    wr('IR.addr.out, TMP0.in')
+    wr('PC.out, ALU.c=0, ALU.r+l+c')
+    wr('ALU.out, PC.in')
+
+
+jsr = '''PC.out, MAR.in, RD
+PC.out, ALU.r+1
+ALU.out, PC.in
+MDR.out, TMP1.in
+R.out, ALU.c=0, ALU.r+l+c
+ALU.out, TMP1.in
+R6.out, ALU.r-1
+ALU.out, R6.in, MAR.in
+PC.out, MDR.in, WR
+TMP1.out, PC.in'''
+
+rts = '''R6.out, MAR.in, RD
+MDR.out, PC.in
+R6.out, ALU.r+1
+ALU.out, R6.in, MAR.in'''
+
+intt = '''R6.out, ALU.r-1
+ALU.out, R6.in, MAR.in
+FLAGS.out, MDR.in
+R6.out, MAR.in, RD
+R6.out, ALU.r-1
+ALU.out, R6.in, MAR.in
+PC.out, MDR.in
+R6.out, MAR.in, RD
+PC.in, HARDWARE_ADDRESS.out'''
+
+iret = '''R6.out, MAR.in, RD
+MDR.out, PC.in
+R6.out, ALU.r+1
+ALU.out, R6.in, MAR.in
+R6.out, MAR.in, RD
+MDR.out, FLAGS.in
+R6.out, ALU.r+1
+ALU.out, R6.in, MAR.in'''
+
+#############################
+
+print('''\
+------------------------------------------
+PDP11-Simplified 2-Bus Micro-instructions
+------------------------------------------
+
+Notes:
+    - Right bus is shortened as `r`, left is `l`.
+    - FLAGS register is connected (out) to `r` and (in) to `l`.
+    - ALU has output register to buffer its output, 
+        where ALU.out outs it to bus.
+    - Instruction fetch micro-instructions are performed one time before each instruction, 
+        they are omitted for clearity.
+    - ALU functions:
+        -- outs zero:                 ALU.zero
+        -- clear carry:               ALU.c=0
+        -- increment:                 ALU.r+1
+        -- decrement:                 ALU.r-1
+        -- add with carry:            ALU.r+l+c
+        -- sub with carry:            ALU.r-l-c
+        -- and:                       ALU.r&l
+        -- or:                        ALU.r|l
+        -- xnor:                      ALU.r(XNOR)l
+        -- not r:                     ALU.~r
+        -- arithmetic shift right:    ALU.asr           (ALU[15]   & ALU[15:1])
+        -- rotate right:              ALU.ror           (ALU[0]    & ALU[15:1])
+        -- rotate left:               ALU.rol           (ALU[14:0] & ALU[15])
+        -- rotate right with carry:   ALU.rrc           (carry     & ALU[15:1])
+        -- rotate left with carry:    ALU.rlc           (ALU[14:0] & carry)
+
+---------------
+Fetch micro-instructions
+---------------
+PC.out, MAR.in, ALU.r+1, RD
+ALU.out, PC.in
+MDR.out, IR.in
+''') 
+
+cicles, memaccess = 4, 1
+
+all_cicles = []
+all_memaccess = []
+unique_signals = set()
+json_out = {'instructions': {}, 'signals': {}}
+lines = []
+
+def get_unique(inp: str):
+    global unique_signals
+    
+    for line in inp.splitlines():
+        for x in line.split(', '):
+            if 'IF' not in x:
+                unique_signals.add(x.strip())
+
+
+def wr(x: str, end='\n'):
+    global cicles, memaccess, lines
+    cicles += 1 + x.count('\n')
+    memaccess += x.count('RD') + x.count('WR')
+    lines.extend(x.splitlines())
+    get_unique(x)
+
+    print(x, end=end)
+
+
+modes = ['R', '(R)+', '-(R)', 'X(R)', '@R',
+              '@(R)+', '@-(R)', '@X(R)']
+
+for (instr, instr_name) in [(mov, 'MOV'), (add, 'ADD'), (adc, 'ADC'),
+                            (sub, 'SUB'), (subc, 'SUBC'), (andd, 'AND'),
+                            (orr, 'OR'), (xnor, 'XNOR'), (cmpp, 'CMP')]:
+    for src in modes:
+        for dst in modes:
+            print()
+            print('-'*15)
+            print(instr_name, src, dst)
+            print('-'*15)
+
+            instr(src, dst)
+            wr('END')
+
+            json_out['instructions'][f'{instr_name} {src} {dst}'] = lines
+            print()
+            print(f'CPU cicles = {cicles}')
+            print(f'MEM access = {memaccess}')
+            all_cicles.append(cicles)
+            all_memaccess.append(memaccess)
+            cicles, memaccess = 4, 1
+            lines = []
+
+for (instr, instr_name) in [(inc, 'INC'), (dec, 'DEC'), (clr, 'CLR'),
+                            (inv, 'INV'), (lsr, 'LSR'), (ror, 'ROR'), (rrc, 'RRC'),
+                            (asr, 'ASR'), (lsl, 'LSL'), (rol, 'ROL'), (rlc, 'RLC')]:
+    for dst in modes:
+        print()
+        print('-'*15)
+        print(instr_name, dst)
+        print('-'*15)
+
+        instr(dst)
+        wr('END')
+
+        json_out['instructions'][f'{instr_name} {src} {dst}'] = lines
+        print()
+        print(f'CPU cicles = {cicles}')
+        print(f'MEM access = {memaccess}')
+        all_cicles.append(cicles)
+        all_memaccess.append(memaccess)
+        cicles, memaccess = 4, 1
+        lines = []
+
+for (name, cond) in [('BR', None),
+                     ('BEQ', '(Z=1)'), ('BNE', '(Z=0)'),
+                     ('BLO', '(C=0)'), ('BLS', '(C=0 or Z=1)'),
+                     ('BHI', '(C=1)'), ('BHS', '(C=1 or Z=1)')]:
+    print()
+    print('-'*15)
+    print(name, '<OFFSET>')
+    print('-'*15)
+
+    branch(cond)
+    wr('END')
+
+    json_out['instructions'][f'{name}'] = lines
+    print()
+    print(f'CPU cicles = {cicles}')
+    print(f'MEM access = {memaccess}')
+    all_cicles.append(cicles)
+    all_memaccess.append(memaccess)
+    cicles, memaccess = 4, 1
+    lines = []
+
+for (name, code) in [('HLT', 'HLT'), ('NOP', ''),
+                     ('JSR X(R)', jsr),
+                     ('RTS', rts),
+                     ('INT', intt),
+                     ('IRET', iret)]:
+    print()
+    print('-'*15)
+    print(name)
+    print('-'*15)
+
+    if code != '':
+        wr(code)
+    if name != 'HLT':
+        wr('END')
+
+    json_out['instructions'][f'{name}'] = lines
+    print()
+    print(f'CPU cicles = {cicles}')
+    print(f'MEM access = {memaccess}')
+    all_cicles.append(cicles)
+    all_memaccess.append(memaccess)
+    cicles, memaccess = 4, 1
+    lines = []
+
+
+print('-'*20)
+print('-'*20)
+print(f'Total CPU cicles = {sum(all_cicles)}')
+print(f'Total MEM access = {sum(all_memaccess)}')
+
+
+print()
+print(f'Average MEM access = {sum(all_memaccess)/len(all_memaccess):3.3f}')
+print(f'CPI = {sum(all_cicles)/len(all_cicles):3.3f}')
+
+
+unique_signals.discard('')
+unique_signals.discard('R.out')
+unique_signals.discard('R.in')
+unique_signals.discard('R6.out')
+unique_signals.discard('R6.in')
+unique_signals.discard('END')
+unique_signals.discard('HLT')
+
+unique_signals = list(unique_signals)
+unique_signals.sort()
+
+print('-'*20)
+print('-'*20)
+print(f'Total Control Signals ({len(unique_signals)}):', '{')
+for sig in unique_signals: print('\t', sig, sep='')
+print('}')
+
+import json, sys
+json_out['signals'] = unique_signals
+print(json.dumps(json_out), file=sys.stderr)
