9ffc8e13bc828df0e698eee538acee5c1d1b49f0
Mahmoud Adas <mido3ds@gmail.com>
2019-12-20 19:05:11 +0200

    added fetch test for iterator

--- test/iterator_tb.vhdl
+++ test/iterator_tb.vhdl
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.common.all;
 
 library vunit_lib;
 context vunit_lib.vunit_context;
@@ -14,17 +15,42 @@ architecture tb of iterator_tb is
     constant CLK_PERD: time    := 1000 ms / CLK_FREQ;
 
     signal clk: std_logic := '0';
+
+    -- iterator
+    signal ir       : std_logic_vector(15 downto 0);     -- in
+    signal flag_regs: std_logic_vector(4 downto 0);      -- in
+    signal address  : std_logic_vector(5 downto 0);      -- in
+
+    signal out_inst : std_logic_vector(26-1 downto 0);   -- out
+    signal NAF      : std_logic_vector(5 downto 0);      -- out
+
+    function cs(adr: std_logic_vector(5 downto 0)) return std_logic_vector is
+        variable i:integer := to_integer(unsigned(adr));
+    begin
+        return CONTROL_STORE(i);
+    end function;
 begin
     clk <= not clk after CLK_PERD / 2;
 
-    -- iterator: entity work.iterator port map ( );
+    iterator: entity work.iterator port map (
+        ir => ir, flag_regs => flag_regs, clk => clk,
+        address => address, out_inst => out_inst, NAF => NAF
+    );
 
     main: process
     begin
         test_runner_setup(runner, runner_cfg);
+        set_stop_level(failure);
 
-        if run("test_case_name") then
+        ir <= (others => '0');
+        flag_regs <= (others => '0');
+        address <= (others => '0');
 
+        if run("fetch") then
+            address <= (others => '0');
+            wait for CLK_PERD;
+            check_equal(NAF, to_vec("000001", 6), "check next address field");
+            check_equal(out_inst, cs("000000"), "check output instructions");
         end if;
 
         test_runner_cleanup(runner);
