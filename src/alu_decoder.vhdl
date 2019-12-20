f069b5882eec10c2f2ca9982fa8d24ffb1307e8d
Evraa <evramyousef@gmail.com>
2019-12-20 20:19:34 +0200

    Starter as function

---
+++ src/alu_decoder.vhdl
@@ -0,0 +1,82 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.ALL;
+
+--IR_SUB: the IR register from 15 to 8 inclusive
+--ALU_MODE: the alu operation
+entity alu_decoder is
+	port (
+		IR_SUB: in std_logic_vector(7 downto 0);
+		ALU_MODE : out std_logic_vector(3 downto 0)
+	);
+end entity;
+
+architecture archALUDEC of alu_decoder is
+begin
+	process (IR_SUB)
+	begin
+		case IR_SUB(7 downto 4) is
+			when "0010" =>
+				--ADD
+				ALU_MODE <= "0000";
+			when "0011" =>
+				--ADC
+				ALU_MODE <= "0000";
+			when "0100" =>
+				--SUB
+				ALU_MODE <= "0001";
+			when "0101" =>
+				--SBC
+				ALU_MODE <= "0001";
+			when "0110" =>
+				--AND
+				ALU_MODE <= "0010";
+			when "0111" =>
+				--OR
+				ALU_MODE <= "0011";
+			when "1000" =>
+				--XNOR
+				ALU_MODE <= "0101";
+			when "1001" =>
+				--CMP --> SUB
+				ALU_MODE <= "0001";	    	
+	    	when OTHERS =>
+	    		--1111????
+	    		case IR_SUB(3 downto 0) is
+					when "0000" =>
+						--INC
+						ALU_MODE <= "1110";
+					when "0001" =>
+						--DEC
+						ALU_MODE <= "1101";
+					when "0010" =>
+						--CLR
+						ALU_MODE <= "1111";
+					when "0011" =>
+						--INV (NOT)
+						ALU_MODE <= "0100";
+					when "0100" =>
+						--LSR
+						ALU_MODE <= "0110";
+					when "0101" =>
+						--ROR
+						ALU_MODE <= "0111";
+					when "0110" =>
+						--RRC
+						ALU_MODE <= "1000";
+					when "0111" =>
+						--ASR
+						ALU_MODE <= "1001";
+					when "1000" =>
+						--LSL
+						ALU_MODE <= "1010";
+					when "1001" =>
+						--ROL
+						ALU_MODE <= "1011";
+					when OTHERS =>
+						--RLC
+						ALU_MODE <= "1100";
+				end case;
+	    end case;
+	end process;
+end architecture;
--- src/starter.vhdl
+++ src/starter.vhdl
@@ -23,87 +23,89 @@ use ieee.numeric_std.ALL;
 	--PC stack pop..... 100100
 	--Flags stack pop.... 100111
 --
-entity starter is
-	generic (n : integer := 16);
+--entity starter is
+--	generic (n : integer := 16);
 
-	--IR: the IR register
-	--MeuAR_ADD:(Micro AR) the addres to where the iterator must start from
-	port (
-		IR: in std_logic_vector(n-1 downto 0);
-		MeuAR_ADD : out std_logic_vector(5 downto 0);
-		clk : in std_logic
-	);
-end entity;
+--	--IR: the IR register
+--	--MeuAR_ADD:(Micro AR) the addres to where the iterator must start from
+--	port (
+--		IR: in std_logic_vector(n-1 downto 0);
+--		MeuAR_ADD : out std_logic_vector(5 downto 0);
+--		clk : in std_logic
+--	);
+--end entity;
 
-architecture archStarter of starter is
+--architecture archStarter of starter is
+--begin
+
+
+
+function starter(IR: std_logic_vector(15 downto 0)) return std_logic_vector is
+	--Variables Declarations
+	MeuAR_ADD : std_logic_vector(37 downto 0);
 begin
-	process (IR, clk)
-	begin
-		if (rising_edge(clk)) then
-			case IR(15 downto 12) is
+	case IR(15 downto 12) is
 				when "0000" =>
 					--BRANCH A
-					MeuAR_ADD <= "101010";
+					MeuAR_ADD := "101010";
 				when "1010" =>
 					--HLT
-					--What should I do?
+					MeuAR_ADD := "000000";
 				when "1011" =>
 					--NOP
-					--What should I do?
+					MeuAR_ADD := "000000";
 				when "1100" =>
 					--BRANCH B
-					MeuAR_ADD <= "101010";
+					MeuAR_ADD := "101010";
 				when "1101" =>
 					--JSR
-					MeuAR_ADD <= "100000";
+					MeuAR_ADD := "100000";
 				when "1110" =>
 					--four choises
 					if (IR(11)= '0' and IR(10) = '0') then
 						--IGNORE IR
-						MeuAR_ADD <= "000000";
+						MeuAR_ADD := "000000";
 					ELSIF (IR(11)= '0' and IR(10) = '1') then
 						--RTS
-						MeuAR_ADD <= "100100";
+						MeuAR_ADD := "100100";
 					ELSIF (IR(11)= '1' and IR(10) = '0') then
 						--itr
-						MeuAR_ADD <= "011101";
+						MeuAR_ADD := "011101";
 					else
 						--iret
-						MeuAR_ADD <= "100100";
+						MeuAR_ADD := "100100";
 					end if;
 				when "1111" =>
 					--ONE OPERAND
 					--Check on IR to know the type of Source 11 10 9
 					case IR(11 downto 9) is
 						when "000" => --Register direct
-							MeuAR_ADD <= "000011";
+							MeuAR_ADD := "000011";
 						when "001" => --Auto-increment
-							MeuAR_ADD <= "000101";
+							MeuAR_ADD := "000101";
 						when "010" => --Auto-decrement
-							MeuAR_ADD <= "000111";
+							MeuAR_ADD := "000111";
 						when "011" => --Indexed
-							MeuAR_ADD <= "001001";
+							MeuAR_ADD := "001001";
 						--when "100" => --Register indirect
 						when OTHERS =>
-							MeuAR_ADD <= "000100";
+							MeuAR_ADD := "000100";
 					end case;
 				when OTHERS =>
 					--TWO OPERANDS
 					--Check on IR to know the type of Destination 5 4 3
 					case IR(5 downto 3) is
 						when "000" => --Register direct
-							MeuAR_ADD <= "010000";
+							MeuAR_ADD := "010000";
 						when "001" => --Auto-increment
-							MeuAR_ADD <= "010010";
+							MeuAR_ADD := "010010";
 						when "010" => --Auto-decrement
-							MeuAR_ADD <= "010100";
+							MeuAR_ADD := "010100";
 						when "011" => --Indexed
-							MeuAR_ADD <= "010110";
+							MeuAR_ADD := "010110";
 						--when "100" => --Register indirect
 						when OTHERS =>
-							MeuAR_ADD <= "010001";
+							MeuAR_ADD := "010001";
 					end case;
-			end case;
-		end if;
-	end process;
-end architecture;
+	end case;
+end function;
