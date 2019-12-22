998a2a09e32d4af348b88a2c01b199a4823aa434
Mahmoud Adas <mido3ds@gmail.com>
2019-12-22 17:34:54 +0200

    hookedup all signals in main_tb

--- test/main_tb.vhdl
+++ test/main_tb.vhdl
@@ -21,17 +21,17 @@ architecture tb of main_tb is
     
     --externals
         -- registers
-        signal mar_enable_in, mar_enable_out, mdr_enable_in, mdr_enable_out : std_logic;
+        signal mar_enable_in, mdr_enable_in, mdr_enable_out : std_logic;
         signal ir_enable_in : std_logic;
-        signal pc_enable_in, pc_enable_out : std_logic;
-        signal flags_enable_in, flags_enable_out : std_logic;
+        signal flags_enable_in, flags_enable_out, flags_clr_carry, flags_set_carry : std_logic;
         signal r_enable_in, r_enable_out : std_logic_vector(7 downto 0);
-        signal tmp_enable_in, tmp_enable_out : std_logic_vector(1 downto 0);
+        signal tmp0_enable_in, tmp0_clr, tmp1_enable_in, tmp1_enable_out : std_logic;
 
         -- ram
         signal rd, wr : std_logic;
 
         -- alu
+        signal alu_mode : std_logic_vector(3 downto 0);
         signal alu_enable : std_logic;
         signal alubuffer_enable_out : std_logic;
 
@@ -40,10 +40,10 @@ architecture tb of main_tb is
         signal controlsignals : std_logic_vector(37 downto 0);
         signal itr_current_adr : std_logic_vector(5 downto 0);
         signal itr_next_adr : std_logic_vector(5 downto 0);
-        signal alu_out : std_logic_vector(15 downto 0);
     --
-
+        
     -- internals
+        signal alu_out : std_logic_vector(15 downto 0);
         signal mar_to_ram, mdr_to_ram : std_logic_vector(15 downto 0);
         signal tmp0_to_alu : std_logic_vector(15 downto 0);
         signal ir_data_out : std_logic_vector(15 downto 0);
@@ -57,28 +57,23 @@ begin
     r : for i in 0 to 7 generate
         ri : entity work.reg generic map (WORD_WIDTH => 16) port map (
             data_in => bbus, enable_in => r_enable_in(i), enable_out => r_enable_out(i),
-            clk => clk, data_out => bbus
+            clk => clk, data_out => bbus, clr => '0'
         );
     end generate;
 
-    pc : entity work.reg generic map (WORD_WIDTH => 16) port map (
-        data_in => bbus, enable_in => pc_enable_in, enable_out => pc_enable_out,
-        clk => clk, data_out => bbus
-    );
-
     tmp0 : entity work.reg generic map (WORD_WIDTH => 16) port map (
-        data_in => bbus, enable_in => tmp_enable_in(0), enable_out => '1',
-        clk => clk, data_out => tmp0_to_alu
+        data_in => bbus, enable_in => tmp0_enable_in, enable_out => '1',
+        clk => clk, data_out => tmp0_to_alu, clr => tmp0_clr
     );
 
     tmp1 : entity work.reg generic map (WORD_WIDTH => 16) port map (
-        data_in => bbus, enable_in => tmp_enable_in(1), enable_out => tmp_enable_out(1),
-        clk => clk, data_out => bbus
+        data_in => bbus, enable_in => tmp1_enable_in, enable_out => tmp1_enable_out,
+        clk => clk, data_out => bbus, clr => '0'
     );
 
     ir : entity work.reg generic map (WORD_WIDTH => 16) port map (
         data_in => bbus, enable_in => ir_enable_in, enable_out => '1',
-        clk => clk, data_out => ir_data_out
+        clk => clk, data_out => ir_data_out, clr => '0'
     );
 
     flags : entity work.flags_reg port map (
@@ -87,14 +82,16 @@ begin
 
         from_alu => alu_to_flags,
         enable_from_alu => alu_enable,
-        always_out => flags_always_out
+        always_out => flags_always_out,
+        clr_carry => flags_clr_carry,
+        set_carry => flags_set_carry
     );
 
     alu : entity work.alu generic map (N =>16) port map (
         clk => clk,
         temp0 => tmp0_to_alu,
         B => bbus,
-        mode => ir_to_alu_mode(ir_data_out(15 downto 8)),
+        mode => alu_mode,
         en => alu_enable,
         flagIn => flags_always_out,
         IR_Check => ir_data_out(12),
@@ -104,14 +101,14 @@ begin
 
     alu_buffer : entity work.reg generic map (WORD_WIDTH => 16) port map (
         data_in => alu_out, enable_in => alu_enable, enable_out => alubuffer_enable_out,
-        clk => clk, data_out => bbus
+        clk => clk, data_out => bbus, clr => '0'
     );
 
     mar : entity work.ram_reg generic map (WORD_WIDTH => 16) port map (
         clk => clk,
         bidir_bus => bbus,
         enable_in => mar_enable_in,
-        enable_out => mar_enable_out,
+        enable_out => '0',
         inout_ram => mar_to_ram,
         enable_in_ram => '0',
         enable_out_ram => '1'
@@ -149,22 +146,28 @@ begin
     controlsignals <= decompress_control_signals(ir_data_out, itr_out_inst);
 
     main: process
-        procedure reset is
+        procedure resetbus is
         begin
-            info("reset");
+            bbus <= (others => 'Z');
+        end procedure;
+
+        procedure resetsignals is
+        begin
+            info("reset signals");
             mar_enable_in <= '0';
-            mar_enable_out <= '0';
             mdr_enable_in <= '0';
             mdr_enable_out  <= '0';
             ir_enable_in  <= '0';
-            pc_enable_in <= '0';
-            pc_enable_out  <= '0';
             flags_enable_in <= '0';
             flags_enable_out  <= '0';
             r_enable_in <= (others => '0');
-            r_enable_out <= (others => '0') ;
-            tmp_enable_in <= (others => '0');
-            tmp_enable_out <= (others => '0') ;
+            r_enable_out <= (others => '0');
+            tmp0_enable_in <= '0';
+            tmp1_enable_in <= '0';
+            tmp1_enable_out <= '0';
+            tmp0_clr <= '0';
+            flags_set_carry  <= '0';
+            flags_clr_carry  <= '0';
     
             -- ram
             rd <= '0';
@@ -172,35 +175,93 @@ begin
     
             -- alu
             alu_enable  <= '0';
+            alu_mode <= (others => '0');
+            alubuffer_enable_out <= '0';
     
             -- iterator
             itr_current_adr <= (others => '0');
 
-            bbus <= (others => 'Z');
+            resetbus;
+        end procedure;
+
+        procedure hookupsignals is
+        begin
+            resetbus;
+            
+            info("hookup signals from iterator");
+
+            mar_enable_in        <= controlsignals(ICS_MAR_IN);
+            mdr_enable_in        <= controlsignals(ICS_MDR_IN); 
+            mdr_enable_out       <= controlsignals(ICS_MDR_OUT); 
+
+            ir_enable_in         <= controlsignals(ICS_IR_IN); 
+
+            flags_enable_in      <= controlsignals(ICS_FLAG_IN); 
+            flags_enable_out     <= controlsignals(ICS_FLAG_OUT); 
+
+            r_enable_in          <= controlsignals(ICS_PC_IN) & controlsignals(ICS_R6_IN downto ICS_R0_IN); 
+            r_enable_out         <= controlsignals(ICS_PC_OUT) & controlsignals(ICS_R6_OUT downto ICS_R0_OUT); 
+
+            tmp0_enable_in       <= controlsignals(ICS_TEMP0_IN); 
+            tmp0_clr             <= controlsignals(ICS_CLR_TEMP0);
+            tmp1_enable_in       <= controlsignals(ICS_TEMP1_IN); 
+            tmp1_enable_out      <= controlsignals(ICS_TEMP1_OUT); 
+    
+            -- ram
+            rd                   <= controlsignals(ICS_RD);
+            wr                   <= controlsignals(ICS_WR);
+    
+            -- alu
+            alu_enable           <= controlsignals(ICS_ALU_ENBL);
+            alu_mode             <= controlsignals(ICS_ALU_3 downto ICS_ALU_0);
+            alubuffer_enable_out <= controlsignals(ICS_ALU_OUT);
+
+            if controlsignals(ICS_SET_CARRY) = '1' then
+                flags_set_carry  <= '1';
+            else
+                flags_set_carry  <= '0';
+            end if;
+
+            if controlsignals(ICS_CLR_CARRY) = '1' then
+                flags_clr_carry  <= '1';
+            else
+                flags_clr_carry  <= '0';
+            end if;
+
+            if controlsignals(ICS_ADRS_OUT) = '1' then
+                if ir_data_out(15 downto 12) = "1101" then -- JSR
+                    bbus <= to_vec(0, 4) & ir_data_out(12 downto 0);
+                else -- BR
+                    bbus <= to_vec(0, 6) & ir_data_out(10 downto 0);
+                end if;
+            end if;
+            
+            -- ignore ICS_PLA_OUT
+
         end procedure;
     begin
         test_runner_setup(runner, runner_cfg);
         set_stop_level(failure);
 
-        reset;
+        resetsignals;
 
         if run("ram") then
             mdr_enable_in <= '1';
             bbus <= (others => '1');
             wait for CLK_PERD;
-            reset;
+            resetsignals;
 
             mar_enable_in <= '1';
             bbus <= to_vec(1);
             wr <= '1';
             wait for CLK_PERD;
-            reset;
+            resetsignals;
 
             mdr_enable_out <= '1';
             rd <= '1';
             wait for CLK_PERD;
             check_equal(bbus, to_vec('1'));
-            reset;
+            resetsignals;
         end if;
 
         if run("fill_reg") then
@@ -208,14 +269,14 @@ begin
                 r_enable_in(i) <= '1';
                 bbus <= to_vec(i);
                 wait for CLK_PERD;
-                reset;
+                resetsignals;
             end loop;
 
             for i in 0 to 7 loop
                 r_enable_out(i) <= '1';
                 wait for CLK_PERD;
                 check_equal(bbus, to_vec(i));
-                reset;
+                resetsignals;
             end loop;
         end if;
 
@@ -223,20 +284,20 @@ begin
             r_enable_in(0) <= '1';
             bbus <= to_vec(512);
             wait for CLK_PERD;
-            reset;
+            resetsignals;
 
             for i in 1 to 7 loop
                 r_enable_in(i) <= '1';
                 bbus <= to_vec(i);
                 wait for CLK_PERD;
-                reset;
+                resetsignals;
             end loop;
 
             for i in 1 to 7 loop
                 r_enable_out(i-1) <= '1';
                 r_enable_in(i) <= '1';
                 wait for CLK_PERD;
-                reset;
+                resetsignals;
             end loop;
             
             info("checking registers");
@@ -244,76 +305,84 @@ begin
                 r_enable_out(i) <= '1';
                 wait for CLK_PERD;
                 check_equal(bbus, to_vec(512));
-                reset;
+                resetsignals;
             end loop;
         end if;
 
         if run("alu_inc") then
-            bbus <= "1111" & "0000" & "00000000";
-            ir_enable_in <= '1';
-            wait for CLK_PERD;
-            reset;
-
             bbus <= to_vec(214);
+            alu_mode <= "1110";
             alu_enable <= '1';
             wait for CLK_PERD;
-            reset;
+            resetsignals;
 
             alubuffer_enable_out <= '1';
             wait for CLK_PERD;
             check_equal(bbus, to_vec(215));
-            reset;
+            resetsignals;
         end if;
 
         if run("alu_add") then
-            bbus <= "0010" & "0000" & "00000000";
-            ir_enable_in <= '1';
-            wait for CLK_PERD;
-            reset;
-
             bbus <= to_vec(50);
-            tmp_enable_in(0) <= '1';
+            tmp0_enable_in <= '1';
             wait for CLK_PERD;
-            reset;
+            resetsignals;
 
             bbus <= to_vec(214);
+            alu_mode <= "0000";
             alu_enable <= '1';
             wait for CLK_PERD;
-            reset;
+            resetsignals;
 
             alubuffer_enable_out <= '1';
             wait for CLK_PERD;
             check_equal(bbus, to_vec(50+214));
-            reset;
+            resetsignals;
         end if;
 
         if run("alu_clears") then
-            bbus <= "1111" & "0010" & "00000000";
-            ir_enable_in <= '1';
-            wait for CLK_PERD;
-            reset;
-
+            alu_mode <= "1111";
             alubuffer_enable_out <= '1';
             wait for CLK_PERD;
             check_equal(bbus, to_vec(0));
-            reset;
+            resetsignals;
         end if;
 
         if run("alu_lsl") then
-            bbus <= "1111" & "1000" & "00000000";
-            ir_enable_in <= '1';
-            wait for CLK_PERD;
-            reset;
-
             bbus <= to_vec('1');
+            alu_mode <= "1010";
             alu_enable <= '1';
             wait for CLK_PERD;
-            reset;
+            resetsignals;
 
             alubuffer_enable_out <= '1';
             wait for CLK_PERD;
             check_equal(bbus, to_vec('1', 15) & '0');
-            reset;
+            resetsignals;
+        end if;
+
+        if run("iterator_fetches") then
+            info("put instruction in ram");
+            bbus <= (others => '0');
+            mar_enable_in <= '1';
+            wait for CLK_PERD;
+            resetsignals;
+
+            bbus <= "0001" & "000000" & "000001";
+            mdr_enable_in <= '1';
+            wr <= '1';
+            wait for CLK_PERD;
+            resetsignals;
+
+            itr_current_adr <= (others => '0');
+            check_equal(itr_next_adr, to_vec(1, 6));
+            wait for CLK_PERD;
+
+            itr_current_adr <= itr_next_adr;
+            wait for CLK_PERD;
+
+            itr_current_adr <= itr_next_adr;
+            wait for CLK_PERD;
         end if;
 
         wait for CLK_PERD/2;
